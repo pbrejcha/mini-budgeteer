7d780a6198e85d2e9a77e74b9b9dc20676a6ff2a
HomeMac <homemac@HomeMacs-MacBook-Air.local>
2014-05-14 21:23:32 -0500

    initial calcuations in place

--- app/app.js
+++ app/app.js
@@ -2,6 +2,19 @@ var budgeteer = angular.module('budgeteer', []);
 
 budgeteer.controller('LoanCtrl', ['$scope', function ($scope) {
     $scope.loan = {};
+
+    $scope.calculateMonthlyPayment = function () {
+        $scope.monthlyPayment = 0;
+
+        var monthlyInterest = ($scope.loan.rate / 100) / 12;
+        var numberOfMonths = 12 * $scope.loan.duration;
+        var denominator = Math.pow((1 + monthlyInterest), (-numberOfMonths));
+
+        if($scope.loan){
+            $scope.monthlyPayment = $scope.loan.principal * (monthlyInterest / (1 - denominator));
+
+        }
+    };
 }]);
 
 angular.bootstrap(document, ['budgeteer']);
